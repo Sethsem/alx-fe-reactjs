ae6bdee1feeda23ac9ceb61eec1603de5cb49fbe
Seth Semevor <sethsem5132@icloud.com>
2025-02-23 22:54:16 +0000

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+    return (
+        <main style={{ padding: '20px', fontSize: '1.2rem', backgroundColor: '#f4f4f4', textAlign: 'center' }}>
+            <p>I love to visit <span style={{ color: 'red', fontWeight: 'bold' }}>New York</span>, <span style={{ color: 'blue', fontWeight: 'bold' }}>Paris</span>, and <span style={{ color: 'green', fontWeight: 'bold' }}>Tokyo</span>.</p>
+        </main>
+    );
+};
+
+export default MainContent;
