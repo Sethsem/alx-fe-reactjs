a1f781dfbf3413c803f70edbb99687225b9033bc
Seth Semevor <sethsem5132@icloud.com>
2025-02-24 13:23:55 +0000

    Refactor Prop Drilling to Use Context API

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -1,24 +1,16 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  return (
-    <nav style={{ padding: "10px", background: "#333", color: "#fff" }}>
-      <ul style={{ display: "flex", gap: "15px", listStyle: "none" }}>
-        <li>
-          <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Home</Link>
-        </li>
-        <li>
-          <Link to="/about" style={{ color: "#fff", textDecoration: "none" }}>About</Link>
-        </li>
-        <li>
-          <Link to="/services" style={{ color: "#fff", textDecoration: "none" }}>Services</Link>
-        </li>
-        <li>
-          <Link to="/contact" style={{ color: "#fff", textDecoration: "none" }}>Contact</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav style={{ background: "#333", padding: "10px" }}>
+            <ul style={{ listStyle: "none", display: "flex", gap: "20px" }}>
+                <li><Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link></li>
+                <li><Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link></li>
+                <li><Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link></li>
+                <li><Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link></li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
