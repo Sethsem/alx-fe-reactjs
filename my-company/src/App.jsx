726998b2798c9080b4880df2b46ad19846dd80fe
Seth Semevor <sethsem5132@icloud.com>
2025-02-24 13:30:01 +0000

    Refactor Prop Drilling to Use Context API

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
 
 function App() {
     return (
