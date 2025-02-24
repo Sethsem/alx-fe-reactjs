e1a2d8c98707d9a906422a824638c4d35014f351
Seth Semevor <sethsem5132@icloud.com>
2025-02-24 15:00:37 +0000

    Build a Simple Company Website with React

--- my-company/src/Component/Navbar.jsx
+++ my-company/src/Component/Navbar.jsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav style={{ background: "#333", padding: "10px" }}>
-            <ul style={{ listStyle: "none", display: "flex", gap: "20px" }}>
-                <li><Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link></li>
-                <li><Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link></li>
-                <li><Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link></li>
-                <li><Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link></li>
+        <nav style={{ background: "#333", padding: "10px", textAlign: "center" }}>
+            <ul style={{ listStyle: "none", display: "flex", justifyContent: "center", padding: 0, margin: 0 }}>
+                <li style={{ margin: "0 15px" }}><Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link></li>
+                <li style={{ margin: "0 15px" }}><Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link></li>
+                <li style={{ margin: "0 15px" }}><Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link></li>
+                <li style={{ margin: "0 15px" }}><Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link></li>
             </ul>
         </nav>
     );
