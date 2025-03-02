6bfd1c2e3246cc6811cbed6aa2bca8683b2b34be
Seth Semevor <sethsem5132@icloud.com>
2025-03-02 16:58:04 +0000

    Implementing User Favorites and Personalized Recipe Recommendations

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,19 +1,23 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
-  recipes: [],
+  recipes: [], // Initial state
   favorites: [],
   recommendations: [],
+
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }), // Function to update recipes
 
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
 
-  deleteRecipe: (id) =>
-    set((state) => ({ recipes: state.recipes.filter((r) => r.id !== id) })),
-
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((r) => (r.id === updatedRecipe.id ? updatedRecipe : r)),
+    })),
+
+  removeRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 
   addFavorite: (recipeId) =>
