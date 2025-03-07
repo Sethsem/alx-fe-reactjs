d6c4f0840baa33c8fb8a9e166cf7b68b6bd3843c
Seth Semevor <sethsem5132@icloud.com>
2025-03-07 23:05:04 +0000

    Advanced Data Handling with React Query

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchPosts = async () => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return response.data;
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, refetch } = useQuery("posts", fetchPosts, {
+    staleTime: 5000, // Cache data for 5 seconds
+  });
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (error) return <p>Error fetching posts: {error.message}</p>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
