d3dcc76c0de5262778b46b94d7aa37db0998f95d
Seth Semevor <sethsem5132@icloud.com>
2025-03-29 19:16:38 +0000

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <Search />
+    <div className="text-center p-4 bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind CSS is working!</h1>
     </div>
   );
 }
